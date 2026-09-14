f203aaff61bbba3fa86485db46c2dd4a7c8b108a
Zachary Kelly <zjkelly19@gmail.com>
2026-09-14 09:32:54 -0400

    Complete thrust implementation and header

--- stream_compaction/thrust.cu
+++ stream_compaction/thrust.cu
@@ -1,28 +1,37 @@
-#include <cuda.h>
-#include <cuda_runtime.h>
 #include <thrust/device_vector.h>
-#include <thrust/host_vector.h>
+#include <thrust/copy.h>
 #include <thrust/scan.h>
 #include "common.h"
 #include "thrust.h"
 
 namespace StreamCompaction {
     namespace Thrust {
-        using StreamCompaction::Common::PerformanceTimer;
-        PerformanceTimer& timer()
-        {
-            static PerformanceTimer timer;
+        Common::PerformanceTimer& timer() {
+            static Common::PerformanceTimer timer;
             return timer;
         }
-        /**
-         * Performs prefix-sum (aka scan) on idata, storing the result into odata.
-         */
+
         void scan(int n, int *odata, const int *idata) {
+            // Avoid creating an input range from an empty array.
+            if (n <= 0) {
+                timer().startGpuTimer();
+                timer().endGpuTimer();
+                return;
+            }
+
+            // These vectors own their GPU memory and free it automatically.
+            // Create them before timing so setup is not measured.
+            thrust::device_vector<int> input(idata, idata + n);
+            thrust::device_vector<int> output(n);
+
+            // Thrust performs the same exclusive sum as our own scans.
             timer().startGpuTimer();
-            // TODO use `thrust::exclusive_scan`
-            // example: for device_vectors dv_in and dv_out:
-            // thrust::exclusive_scan(dv_in.begin(), dv_in.end(), dv_out.begin());
+            thrust::exclusive_scan(input.begin(), input.end(), output.begin());
             timer().endGpuTimer();
+            checkCUDAError("thrust scan");
+
+            // Bring the answer back to the CPU after timing the scan.
+            thrust::copy(output.begin(), output.end(), odata);
         }
     }
 }
--- stream_compaction/thrust.h
+++ stream_compaction/thrust.h
@@ -6,6 +6,7 @@ namespace StreamCompaction {
     namespace Thrust {
         StreamCompaction::Common::PerformanceTimer& timer();
 
+        // Read n CPU values and write their exclusive prefix sums to odata.
         void scan(int n, int *odata, const int *idata);
     }
 }
